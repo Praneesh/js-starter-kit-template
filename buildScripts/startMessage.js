034ae0ba93c75d739a467dbd3e123749ee571256
Praneesh Kumar Yadav <pranuvitmsse05@gmail.com>
2017-06-13 14:59:52 +0530

    Babel and ES5->ES6 changes in the code.

--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,3 +1,12 @@
-var chalk = require('chalk');
+/* ES5 Style Implementation
+var chalk = require('chalk');*/
+
+/*
+    ES6 Style Implementation
+    This would now be transpiled by Babel
+    Make sure to run 'babel-node' instead of just 'node' for running this script
+*/
+
+import chalk from 'chalk';
 
 console.log(chalk.green('Starting application in dev mode !!'));

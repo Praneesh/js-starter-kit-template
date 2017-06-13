290ebe911eed3b802ec494492def16c9e6faa938
Praneesh Kumar Yadav <pranuvitmsse05@gmail.com>
2017-06-13 15:46:22 +0530

    Included Webpack for bundling content. Added sample index files in src folder to check if it works.

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './index.css';
+
+import numeral from 'numeral';
+
+const packageValue = numeral(1000).format('$0,0.00');
+console.log(`I would pay ${packageValue} for this awesome package !`);  // ` is a backtick formatter in ES6, which helps to subsistute string values

2833b97d4b1b6955e5bd190c8641dddaab7172c9
Praneesh Kumar Yadav <pranuvitmsse05@gmail.com>
2017-06-19 00:23:40 +0530

    Hashing for Cache Bursting and CSS Minifaction seperation so that CSS is not included in the bundled JS.

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,6 +1,8 @@
 import path from 'path';
 import webpack from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
+import WebpackMd5Hash from 'webpack-md5-hash';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
 
 export default{
     debug: true,
@@ -14,9 +16,19 @@ export default{
     output:{
         path: path.resolve(__dirname,'dist'),
         publicPath:'/',
-        filename: '[name].js'   //This tells webpack to use the names we used in "entry" module above (vendor, main etc.)
+        filename: '[name].[chunkhash].js'
+        // This tells webpack to use the names we used in "entry" module above (vendor, main etc.)
+        // The 'chunkhash' variable is released by WebpackMd5Hash
     },
     plugins:[
+        // Generate an external CSS file with a hash in the file name
+        new ExtractTextPlugin('[name].[contenthash].css'),
+
+        // Hash the files using MD5 so that their names change when the content changes.
+        // This would helpto burst cache (cache-bursting) so that the end user's browsers would
+        // be forced to download the changed the files from server, instead of serving them from cache
+        new WebpackMd5Hash(),
+
         // CommonChunksPlugin to create a seperate bundle
         // of vendor libraries so that they are cached seperately.
         new webpack.optimize.CommonsChunkPlugin({
@@ -50,7 +62,7 @@ export default{
     module:{
         loaders:[
             {test: /\.js$/, exclude: /node_modules/, loaders: ['babel']},
-            {test: /\.css$/,loaders:['style','css']}
+            {test: /\.css$/,loader: ExtractTextPlugin.extract('css?sourceMap')}
         ]
     }
 }
